c6429e501162c7494591034e04a2f5e179e8abbc
kk5190 <kk5190krrish@gmail.com>
2019-08-11 21:28:16 +0530

    Updates

---
+++ precache-manifest.67863a5ead8d62feadea44552c434a2c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1ef4e73f824f6341f2c18f5f62edf94",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "93a5d490ba73c39790ea",
+    "url": "/monsters-rolodex/static/css/main.b9bd754b.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/monsters-rolodex/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "93a5d490ba73c39790ea",
+    "url": "/monsters-rolodex/static/js/main.8e63c796.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
